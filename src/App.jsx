f0e718c80d0ba1549cd8b8a8f426a58bb107a6b7
Nicolas Vallejo <nicolas.vallejo@novexisconsulting.com>
2024-09-03 12:50:52 -0300

    iniciando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-900 text-center text-4xl font-bold text-white">
+      <h1>
+        React <br />
+        +
+        <br />
+        Tailwind
+        <br />
+        +
+        <br />
+        Formateador Prettier-Tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
