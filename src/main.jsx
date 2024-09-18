5ac38768a852f130c85e24f51462ae755c7a4916
Victor <utrera1695@gmail.com>
2024-09-18 12:15:26 -0400

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Router/index.jsx";
+import { router } from "./router/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />,
