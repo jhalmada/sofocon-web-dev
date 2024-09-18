77c9134d2e21c0fa53581d787db05f145f65612f
Hemer Meza <hemer.meza@novexisconsulting.com>
2024-09-18 17:11:30 -0500

    add endpoint login

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,17 +1,51 @@
 import Input from "../components/inputs/Input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import IconEye from "../assets/icons/IconEye.svg";
 import IconEyeSlash from "../assets/icons/IconEyeSlash.svg";
+import { useState } from "react";
+import { BASE_URL } from "../Utils/Constants";
+import useApiRequest from "../Hooks/useApiRequest";
+import { HOME_ROUTE } from "../Utils/Constants";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { sendRequest, error, data, loading } = useApiRequest(BASE_URL);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Llamada a la API de login
+    await sendRequest({
+      endpoint: "/auth/login",
+      method: "POST",
+      body: {
+        email,
+        password,
+      },
+      headers: {
+        "origin-login": "dashboard",
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (data) {
+      localStorage.setItem("token", data.accessToken);
+      navigate(HOME_ROUTE);
+    }
+  };
   return (
-    <div className="w-full">
+    <form className="w-full" onSubmit={handleSubmit}>
       <Input
         placeholder={"Escribe tu correo"}
         label={"Dirección de correo"}
         msjError={
           "*Este campo debe contener una direccion de correo válida vinculada a la plataforma."
         }
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        errorApi={error}
       />
       <Input
         placeholder={"Escribe tu contraseña"}
@@ -22,14 +56,19 @@ const LoginPage = () => {
         type={"password"}
         icon1={IconEye}
         icon2={IconEyeSlash}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        errorApi={error}
       />
       <div className="flex flex-col items-center">
-        <Link to={"/inicio"} className="mt-[6.25rem]">
-          <button className="shadow-gray-500 h-11 w-[9.1885rem] rounded-[1.25rem] bg-[#D70000] font-roboto text-sm font-medium uppercase text-white shadow-md">
-            INGRESAR
-          </button>
-        </Link>
-
+        <button
+          type="submit"
+          disabled={loading} // Deshabilita el botón si está cargando
+          className={`shadow-gray-500 h-11 w-[9.1885rem] rounded-[1.25rem] font-roboto text-sm font-medium uppercase text-white shadow-md ${loading ? "bg-gray-400 cursor-not-allowed" : "bg-[#D70000]"}`} // Cambia el estilo si está deshabilitado
+        >
+          INGRESAR
+        </button>
+        {error && <p>{error}</p>}
         <Link
           to={"recuperar-contraseña"}
           className="mt-5 font-roboto text-xs font-medium text-[#555]"
@@ -37,7 +76,7 @@ const LoginPage = () => {
           ¿Olvidaste tu contraseña?
         </Link>
       </div>
-    </div>
+    </form>
   );
 };
 
