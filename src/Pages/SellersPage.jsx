32db0570c484be25cfaad067ce2c24c2b47c91db
Nicolas Vallejo <nicolas.vallejo@novexisconsulting.com>
2024-09-18 16:44:31 -0300

    mr nicolas to hemer

--- src/Pages/SellersPage.jsx
+++ src/Pages/SellersPage.jsx
@@ -1,15 +1,7 @@
 import ChevronLeftIcon from "../assets/icons/chevron-left.svg";
 import { Link } from "react-router-dom";
-import Select from "../components/selects/Select";
-import Input from "../components/inputs/Input";
-import Button from "../components/buttons/Button";
-import ArrowRightIcon from "../assets/icons/arrow-right.svg";
 
 const SellersPage = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Formulario enviado");
-  };
   return (
     <div className="flex h-full flex-col justify-between overflow-auto bg-gray">
       <div className="flex-grow p-6">
@@ -25,39 +17,18 @@ const SellersPage = () => {
         </div>
 
         <h1 className="mb-5 text-xl font-medium leading-6 text-black_m">
-          Usuarios
+          Vendedores
         </h1>
 
         <div className="flex items-center justify-between">
           <div className="flex">
             <span className="w-36 cursor-pointer rounded-t-lg bg-white p-4 text-center text-md font-medium leading-6 shadow-t">
-              Roles
+              Vendedores
             </span>
           </div>
         </div>
 
-        <form
-          onSubmit={handleSubmit}
-          className="rounded-tr-lg bg-white px-14 py-5 shadow-t"
-        >
-          <div className="space-y-6">
-            <Input
-              label={"Nombre del rol"}
-              placeholder={"Escribe el nombre del rol..."}
-            />
-
-            <Select option={"Permisos"} />
-          </div>
-        </form>
-        <div className="flex justify-end py-6">
-          <Button
-            text={"GUARDAR"}
-            onClick={handleSubmit}
-            color={"save"}
-            type={"submit"}
-            icon={ArrowRightIcon}
-          />
-        </div>
+        <form className="rounded-tr-lg bg-white px-14 py-5 shadow-t"></form>
       </div>
     </div>
   );
