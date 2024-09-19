2a83c1b2a0a90c8b2c811249f5cee5e353a16c57
Nicolas Vallejo <nicolas.vallejo@novexisconsulting.com>
2024-09-19 17:06:20 -0300

    update login with modal

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -2,21 +2,22 @@ import Input from "../components/inputs/Input";
 import { Link, useNavigate } from "react-router-dom";
 import IconEye from "../assets/icons/IconEye.svg";
 import IconEyeSlash from "../assets/icons/IconEyeSlash.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BASE_URL } from "../Utils/Constants";
 import useApiRequest from "../Hooks/useApiRequest";
 import { HOME_ROUTE } from "../Utils/Constants";
+import ReusableModal from "../components/modals/ReusableModal";
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { sendRequest, error, data, loading } = useApiRequest(BASE_URL);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Llamada a la API de login
     await sendRequest({
       endpoint: "/auth/login",
       method: "POST",
@@ -33,15 +34,28 @@ const LoginPage = () => {
     if (data) {
       localStorage.setItem("token", data.accessToken);
       navigate(HOME_ROUTE);
+    } else if (error) {
+      setIsModalOpen(true);
     }
   };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    if (error) {
+      setIsModalOpen(true);
+    }
+  }, [error]);
+
   return (
     <form className="w-full" onSubmit={handleSubmit}>
       <Input
         placeholder={"Escribe tu correo"}
         label={"Dirección de correo"}
         msjError={
-          "*Este campo debe contener una direccion de correo válida vinculada a la plataforma."
+          "*Este campo debe contener una dirección de correo válida vinculada a la plataforma."
         }
         value={email}
         onChange={(e) => setEmail(e.target.value)}
@@ -63,12 +77,23 @@ const LoginPage = () => {
       <div className="flex flex-col items-center">
         <button
           type="submit"
-          disabled={loading} // Deshabilita el botón si está cargando
-          className={`shadow-gray-500 h-11 w-[9.1885rem] rounded-[1.25rem] font-roboto text-sm font-medium uppercase text-white shadow-md ${loading ? "bg-gray-400 cursor-not-allowed" : "bg-[#D70000]"}`} // Cambia el estilo si está deshabilitado
+          disabled={loading}
+          className={`shadow-gray-500 h-11 w-[9.1885rem] rounded-[1.25rem] font-roboto text-sm font-medium uppercase text-white shadow-md ${loading ? "bg-gray-400 cursor-not-allowed" : "bg-[#D70000]"}`}
         >
           INGRESAR
         </button>
-        {error && <p>{error}</p>}
+        {error && (
+          <ReusableModal
+            isOpen={isModalOpen}
+            onClose={handleCloseModal}
+            title="Error de inicio de sesión"
+            variant="confirmation"
+            buttons={["accept"]}
+            onAccept={handleCloseModal}
+          >
+            <p>{error}</p>
+          </ReusableModal>
+        )}
         <Link
           to={"recuperar-contraseña"}
           className="mt-5 font-roboto text-xs font-medium text-[#555]"
