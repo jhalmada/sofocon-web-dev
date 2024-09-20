9929040b2c630c20eae1053ff9347d56236a3dc3
Victor <utrera1695@gmail.com>
2024-09-20 13:11:15 -0400

    remove errors

--- src/services/user/user.service.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import * as usersRoutes from "./user.routes.js";
-
-export class UserService {
-    static getAllUsersApi = async () => await axios.get(usersRoutes.getUsers);
-}
