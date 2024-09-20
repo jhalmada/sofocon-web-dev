ca5e4d75d452c8995065ccd4019088a1d9a989b2
Nicolas Vallejo <nicolas.vallejo@novexisconsulting.com>
2024-09-20 17:06:28 -0300

    update default headers axios

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/index.jsx";
-import {BASE_URL} from "./utils/Constants.js";
+import { BASE_URL } from "./utils/Constants.js";
 import axios from "axios";
-import {SOFOCON_JWT_TOKEN} from "./utils/Constants.js";
+import { SOFOCON_JWT_TOKEN } from "./utils/Constants.js";
 
 axios.defaults.baseURL = BASE_URL;
+axios.defaults.headers["origin-login"] = "dashboard";
 
-axios.interceptors.request.use(request => {
+axios.interceptors.request.use(
+  (request) => {
     const accessToken = localStorage.getItem(SOFOCON_JWT_TOKEN);
     if (accessToken) {
-        request.headers['Authorization'] = `Bearer ${accessToken}`;
+      request.headers["Authorization"] = `Bearer ${accessToken}`;
     }
     return request;
-}, error => {
+  },
+  (error) => {
     return Promise.reject(error);
-});
+  },
+);
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />,
