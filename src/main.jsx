fe075cb83188860691d0a9b5a1331e5970fb5948
Hemer Meza <hemer.meza@novexisconsulting.com>
2024-10-18 02:20:37 -0500

    new funcionality

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,39 @@ import axios from "axios";
 import { SOFOCON_JWT_TOKEN } from "./utils/Constants.js";
 import { NextUIProvider } from "@nextui-org/react";
 import AuthProvider from "./hooks/context/AuthContext.jsx";
+import moment from "moment/moment.js";
+import { AuthService } from "./services/auth/auth.service.js";
 
 axios.defaults.baseURL = BASE_URL;
 axios.defaults.headers["origin-login"] = "dashboard";
 
+const parseJwt = (token) => {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join(""),
+  );
+
+  return JSON.parse(jsonPayload);
+};
+
 axios.interceptors.request.use(
-  (request) => {
+  async (request) => {
     const accessToken = localStorage.getItem(SOFOCON_JWT_TOKEN);
     if (accessToken) {
-      request.headers["Authorization"] = `Bearer ${accessToken}`;
+      const payLoad = parseJwt(accessToken);
+      const date = moment(payLoad.exp * 1000);
+      // if (moment().isAfter(date)) {
+      //   await AuthService.refreshTokenAction();
+      // }
+      request.headers["Authorization"] =
+        `Bearer ${localStorage.getItem(SOFOCON_JWT_TOKEN)}`;
     }
     return request;
   },
@@ -23,6 +47,27 @@ axios.interceptors.request.use(
     return Promise.reject(error);
   },
 );
+
+// axios.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     const originalRequest = error.config;
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+//       try {
+//         return axios(originalRequest);
+//       } catch (_error) {
+//         if (_error.response && _error.response.data) {
+//           return Promise.reject(_error.response.data);
+//         }
+//         return Promise.reject(_error);
+//       }
+//     }
+//     return Promise.reject(error);
+//   },
+// );
 
 createRoot(document.getElementById("root")).render(
   <NextUIProvider>
--- src/services/auth/auth.service.js
+++ src/services/auth/auth.service.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 import * as authRoutes from './auth.routes.js';
+import {
+  BASE_URL,
+  SOFOCON_JWT_REFRESH_TOKEN,
+  SOFOCON_JWT_TOKEN,
+} from "../../utils/Constants.js";
 
 export class AuthService {
   /**
@@ -9,6 +14,29 @@ export class AuthService {
    */
   static loginAction = async (params) =>
     await axios.post(authRoutes.login, params);
+
   static recoveryPasswordAction = async (params) =>
     await axios.post(authRoutes.recovery, params);
+
+  static refreshTokenAction = async () => {
+    try {
+      const instance = axios.create({ baseURL: BASE_URL });
+      const response = instance.get(authRoutes.refreshToken, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(SOFOCON_JWT_REFRESH_TOKEN)}`,
+        },
+      });
+      localStorage.setItem(SOFOCON_JWT_TOKEN, response.data.access_token);
+      localStorage.setItem(
+        SOFOCON_JWT_REFRESH_TOKEN,
+        response.data.refresh_token,
+      );
+      return response;
+    } catch (error) {
+      localStorage.removeItem(SOFOCON_JWT_TOKEN);
+      localStorage.removeItem(SOFOCON_JWT_REFRESH_TOKEN);
+      window.location.href = "/login";
+      return error;
+    }
+  };
 }
